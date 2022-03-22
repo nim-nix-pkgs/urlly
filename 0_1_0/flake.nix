0743648e211cd5e5dad2fefde63835ffb84e6048
hugosenari <hugosenari@gmail.com>
2022-03-22 00:38:54 -0300

    chore: re-index ../nimpkgs/u/urlly

--- 0_1_0/flake.nix
+++ 0_1_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-urlly-0_1_0.flake = false;
   inputs.src-urlly-0_1_0.owner = "treeform";
-  inputs.src-urlly-0_1_0.ref   = "refs/tags/0.1.0";
+  inputs.src-urlly-0_1_0.ref   = "0_1_0";
   inputs.src-urlly-0_1_0.repo  = "urlly";
   inputs.src-urlly-0_1_0.type  = "github";
   
