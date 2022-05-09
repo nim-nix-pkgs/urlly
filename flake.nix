dd2ed4836ae82e447ef4417829b26a9c7223a33e
hugosenari <hugosenari@gmail.com>
2022-05-09 20:45:02 -0300

    chore: re index urlly

--- flake.nix
+++ flake.nix
@@ -7,46 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."urlly-master".dir   = "master";
-  inputs."urlly-master".owner = "nim-nix-pkgs";
-  inputs."urlly-master".ref   = "master";
-  inputs."urlly-master".repo  = "urlly";
-  inputs."urlly-master".type  = "github";
-  inputs."urlly-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."urlly-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."uriiy-0_1_0".dir   = "0_1_0";
-  inputs."uriiy-0_1_0".owner = "nim-nix-pkgs";
-  inputs."uriiy-0_1_0".ref   = "master";
-  inputs."uriiy-0_1_0".repo  = "uriiy";
-  inputs."uriiy-0_1_0".type  = "github";
-  inputs."uriiy-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."uriiy-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."urlly-0_2_0".dir   = "0_2_0";
-  inputs."urlly-0_2_0".owner = "nim-nix-pkgs";
-  inputs."urlly-0_2_0".ref   = "master";
-  inputs."urlly-0_2_0".repo  = "urlly";
-  inputs."urlly-0_2_0".type  = "github";
-  inputs."urlly-0_2_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."urlly-0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."urlly-0_3_0".dir   = "0_3_0";
-  inputs."urlly-0_3_0".owner = "nim-nix-pkgs";
-  inputs."urlly-0_3_0".ref   = "master";
-  inputs."urlly-0_3_0".repo  = "urlly";
-  inputs."urlly-0_3_0".type  = "github";
-  inputs."urlly-0_3_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."urlly-0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."urlly-1_0_0".dir   = "1_0_0";
-  inputs."urlly-1_0_0".owner = "nim-nix-pkgs";
-  inputs."urlly-1_0_0".ref   = "master";
-  inputs."urlly-1_0_0".repo  = "urlly";
-  inputs."urlly-1_0_0".type  = "github";
-  inputs."urlly-1_0_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."urlly-1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
